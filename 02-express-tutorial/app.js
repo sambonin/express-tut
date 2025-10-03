908e9f4aec5b73730209b0b14e90f082d669595d
Sam Bonin <sambonin777@gmail.com>
2025-10-03 14:35:25 -0500

    Api/SearchQuerys

--- 02-express-tutorial/app.js
+++ 02-express-tutorial/app.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const path = require('path');
 const app = express();
-const {products} = require('./data')
+const {products, products} = require('./data')
 
 
 app.use( express.static('./public'))
@@ -11,7 +11,50 @@ app.get('/', (req, res) => {
 })
 
 app.get('/api/products',(req, res) => {
+    const newProducts = products.map((products)=> {
+        const {id,image,name} = products;
+        return {id, name, image}
+    })
+
     res.json(products)
+})
+
+app.get('/api/products/:productID',(req, res) => {
+    const {productID} = req.params;
+    const singleProduct = products.find(
+        (products) => product.id === Number(productID)
+    )
+    if(!singleProduct) {
+        return res.status(404).send('Product does not exist')
+    }
+
+    res.json(singleProduct)
+})
+
+app.get('/api/products/:productID/reviews/:reviewID', (req, res) => {
+
+    const {search, limit} = req.query
+    let sortedProducts = [...products];
+
+    if(search) {
+        sortedProducts = sortedProducts.filter((products)=> {
+            return products.name.startsWith(search)
+        })
+    }
+    if (limit) {
+        sortedProducts = sortedProducts.slice(0,Number(limit))
+    }
+    if(sortedProducts.length < 1) {
+        res.status(200).json({success:true, data: []})
+    }
+    res.status(200).json(sortedProducts)
+    res.send(req.params)
+})
+
+app.get('/api/v1/query', (req, res) => {
+    console.log(req.query)
+    res.send('hello world')
+
 })
 
 app.listen(2000, (req, res) => {
